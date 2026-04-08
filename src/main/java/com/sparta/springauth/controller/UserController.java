969b8116111a7485e44e30b2ba4842e681bddd04
qkrwns1478 <qkrwns1478@gmail.com>
2026-04-08 16:52:52 +0900

    Spring Security JWT 로그인

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -36,14 +36,4 @@ public class UserController {
         return "redirect:/api/user/login-page";
     }
 
-    @PostMapping("/user/login")
-    public String login(LoginRequestDto loginRequestDto, HttpServletResponse res) {
-        try {
-            userService.login(loginRequestDto, res);
-        } catch (Exception e) {
-            return "redirect:/api/user/login-page?error";
-        }
-
-        return "redirect:/";
-    }
 }
